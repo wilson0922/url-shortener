14d0f93ba7e2469b7b4acf34b9d0e8a8efa4279e
tree70373 <tree70373@gmail.com>
2022-09-16 09:36:47 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
 res.send('Hello World!')
