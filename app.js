e4107699cd90fb9fc3c2897f8305d938298bd552
tree70373 <tree70373@gmail.com>
2022-09-16 10:14:20 +0800

    set handlebars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const exphbs =require('express-handlebars')
+
 const app = express()
 const port = 3000
 
@@ -16,9 +18,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs',exphbs({defaultLayout:'main', extname:'.hbs'}))
+app.set('view engine','hbs')
 
 app.get('/', (req, res) => {
-res.send('Hello World!')
+res.render('index')
 })
 
 app.listen(port, () => {
