33121898e13c3301069cdeeabf14be4e208f334d
tree70373 <tree70373@gmail.com>
2022-09-18 02:06:57 +0800

    refactor and add image

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs =require('express-handlebars')
+require('./config/mongoose')
 
 const URL = require('./models/URL')
 const shortURL = require('./utils/shortURL')
 const app = express()
 const port = 3000
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs',exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine','hbs')
